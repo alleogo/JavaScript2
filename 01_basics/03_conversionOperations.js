bd408d43dbe4469d69fa5ddfd17b6f115f724b28
Vipul <rvipuleo@gmail.com>
2025-01-20 11:45:29 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,75 @@
+// let score1 = 33
+// console.log(typeof score1)
+// let value1 = Number(score1)
+// console.log(typeof value1)
+// console.log(value)
+
+// let score2 = "33hari"
+// console.log(typeof score2)
+// let value2 = Number(score2)
+// console.log(typeof value2)
+// console.log(value2)
+
+// let score3 = null
+// console.log(typeof score3)
+// let value3 = Number(score3)
+// console.log(typeof value3)
+// console.log(value3)
+
+// let score4 = undefined
+// console.log(typeof score4)
+// let value4 = Number(score4)
+// console.log(typeof value4)
+// console.log(value4)
+
+// let score5 = true
+// console.log(typeof score5)
+// let value5 = Number(score5)
+// console.log(typeof value5)
+// console.log(value5)
+
+// let score6 = false
+// console.log(typeof score6)
+// let value6 = Number(score6)
+// console.log(typeof value6)
+// console.log(value6)
+
+// let score7 = "false"
+// console.log(typeof score7)
+// let value7 = Number(score7)
+// console.log(typeof value7)
+// console.log(value7)
+
+// 33 => 33
+// "33hari" => NaN(Not a Number)
+// null => 0
+// undefined => NaN
+// "string" => NaN
+// true => 1 ; false => 0
+
+// ********************************************** //
+
+// let is1 = true
+// let boolIs1 = Boolean(is1)
+// console.log(boolIs1)
+
+// let is2 = false
+// let boolIs2 = Boolean(is2)
+// console.log(boolIs2)
+
+// let is3 = ""
+// let boolIs3 = Boolean(is3)
+// console.log(boolIs3)
+
+// let is4 = "string"
+// let boolIs4 = Boolean(is4)
+// console.log(boolIs4)
+
+// 1 => true; 0 => false
+// "" => false
+// "string" => true
+
+let number = 234
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)

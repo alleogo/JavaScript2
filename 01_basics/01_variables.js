c6dabd94d9e63c9329f0a9f60e36c6f3addd6a39
Vipul <rvipuleo@gmail.com>
2025-01-20 06:54:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// prefer no to use 'var', because of issue in block scope and functional scope
+const id = 23523
+var mail = "kuchhbhi@lmaomail.com"
+let name = "Hari"
+city = "Prayag"
+let variable    // undefined value
+
+console.log(mail)
+console.table([mail, name, city, variable])

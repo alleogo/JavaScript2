16b18c96fe3a83dcd19f28924c90db485119cfe1
Vipul <rvipuleo@gmail.com>
2025-01-20 06:54:40 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+// alert(10 + 8) // we're using nodeJS, not browser
+
+console.log(10 + 8)
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined // undefined is different from null value.
+// symbol
+// object
+
+let a = 18
+let b = "string"
+let c = true
+let d = 27572798279827985
+let e
+let f = null
+
+console.table([typeof(null), typeof(undefined), typeof(a), typeof(b), typeof(c), typeof(d), typeof(e), typeof(f)])
+

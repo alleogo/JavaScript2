98216f34e370967b07952f4517b6a074c6ad4e45
Vipul <rvipuleo@gmail.com>
2025-01-29 10:25:06 +0000

    whileDoWhile

---
+++ 05_iterations/while_do_while.js
@@ -0,0 +1,11 @@
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`)
+//     score++
+// } while(score <= 10)
+
+let score2 = 11 // still first iteration execute ho jaegi
+do {
+    console.log(`Score2 is ${score2}`)
+    score2++
+} while(score2 <= 10)
